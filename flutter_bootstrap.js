3df59f20ab6b1eeb4719d5a42f9fc566095c1366
ALFIAN-code <allvvnt@gmail.com>
2025-02-12 18:34:00 +0700

    resized image

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3244727631"
+    serviceWorkerVersion: "1176665826"
   }
 });
